14e9041c9d1800943db5ff0be550227219e28881
Damian Janowski <damian.janowski@gmail.com>
2009-07-24 19:53:56 -0300

    Updated monk-glue.

--- vendor/monk-glue/lib/monk/glue/reloader.rb
+++ vendor/monk-glue/lib/monk/glue/reloader.rb
@@ -2,9 +2,12 @@ require "rack/reloader"
 
 # TODO Add documentation.
 class Monk::Glue::Reloader < Rack::Reloader
+  def initialize(app, cooldown = 0, backend = Stat)
+    super(app, cooldown, backend)
+  end
+
   def safe_load(file, mtime, stderr = $stderr)
-    super
     Main.reset!
-    super(Main.app_file, mtime, stderr)
+    super(file, mtime, stderr)
   end
 end
