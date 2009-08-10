9ee15d3b8f5b97a675fc768cb5fce945c915d4fa
Damian Janowski <damian.janowski@gmail.com>
2009-08-10 17:16:59 -0300

    Updated monk-glue.

--- vendor/monk-glue/lib/monk/glue/reloader.rb
+++ vendor/monk-glue/lib/monk/glue/reloader.rb
@@ -7,8 +7,11 @@ class Monk::Glue::Reloader < Rack::Reloader
   end
 
   def safe_load(file, mtime, stderr = $stderr)
-    $LOADED_FEATURES.delete_if {|path| path =~ /^#{root_path}/ }
+    $LOADED_FEATURES.delete(root_path("init.rb"))
+    $LOADED_FEATURES.delete_if {|path| path =~ /^#{root_path("app")}/ }
+
     Main.reset!
+
     super(Main.app_file, mtime, stderr)
   end
 end
