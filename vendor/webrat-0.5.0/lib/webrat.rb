517eb897095d492c5a0a56a0e0aa3ff306bb02db
Michel Martens <michel@soveran.com>
2009-08-25 12:53:49 -0700

    Updated dependencies

--- vendor/webrat-0.5.0/lib/webrat.rb
+++
@@ -1,9 +0,0 @@
-module Webrat
-  # The common base class for all exceptions raised by Webrat.
-  class WebratError < StandardError
-  end
-end
-
-require "nokogiri"
-require "webrat/core/xml/nokogiri"
-require "webrat/core"
