d7fd1c2f7ebe7eeedef90d46ccb5a650ebcdc4bf
Damian Janowski <damian.janowski@gmail.com>
2009-08-25 18:57:55 -0700

    Renamed story test.

--- test/stories/site_test.rb
+++ test/stories/site_test.rb
@@ -1,6 +1,6 @@
 require "stories_helper"
 
-class HomeTest < Test::Unit::TestCase
+class SiteTest < Test::Unit::TestCase
   story "As a developer I want to see the homepage so I know Monk is correctly installed" do
     scenario "A visitor goes to the homepage" do
       visit "/"
