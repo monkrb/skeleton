2ecde4c89b2b1d0cb45270f0ea16dec14357c170
Michel Martens <michel@soveran.com>
2009-09-07 11:44:47 -0300

    Updated Ohm

--- vendor/ohm-0.0.18/lib/ohm.rb
+++ vendor/ohm-0.0.18/lib/ohm.rb
@@ -385,10 +385,7 @@ module Ohm
 
     def initialize(attrs = {})
       @_attributes = Hash.new {|hash,key| hash[key] = read_remote(key) }
-
-      attrs.each do |key, value|
-        send(:"#{key}=", value)
-      end
+      update_attributes(attrs)
     end
 
     def new?
@@ -398,16 +395,33 @@ module Ohm
     def create
       return unless valid?
       initialize_id
-      create_model_membership
-      add_to_indices
-      save!
+
+      mutex do
+        create_model_membership
+        add_to_indices
+        save!
+      end
     end
 
     def save
       return create if new?
       return unless valid?
-      update_indices
-      save!
+
+      mutex do
+        update_indices
+        save!
+      end
+    end
+
+    def update(attrs)
+      update_attributes(attrs)
+      save
+    end
+
+    def update_attributes(attrs)
+      attrs.each do |key, value|
+        send(:"#{key}=", value)
+      end
     end
 
     def delete
@@ -455,6 +469,14 @@ module Ohm
       other.key == key
     rescue ModelIsNew
       false
+    end
+
+    # Lock the object before ejecuting the block, and release it once the block is done.
+    def mutex
+      lock!
+      yield
+      unlock!
+      self
     end
 
   protected
@@ -552,6 +574,18 @@ module Ohm
 
     def index_key_for(attrs, values)
       self.class.key *(attrs + self.class.encode_each(values))
+    end
+
+    # Lock the object so no other instances can modify it.
+    # @see Model#mutex
+    def lock!
+      lock = db.setnx(key(:_lock), 1) until lock == 1
+    end
+
+    # Release the lock.
+    # @see Model#mutex
+    def unlock!
+      db.del(key(:_lock))
     end
   end
 end
