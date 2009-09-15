1d21ccec452e8d0bd5c27e388ec567bb70c1ed18
Aupajo <pete@metanation.com>
2009-09-14 17:23:09 -0700

    Fixed spelling in CSS test.

--- test/routes/css_test.rb
+++ test/routes/css_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class CssTest < Test::Unit::TestCase
-  test "renders the default stylsheet" do
+  test "renders the default stylesheet" do
     get "/css/main.css"
     assert_equal "text/css;charset=UTF-8", last_response.content_type
   end
