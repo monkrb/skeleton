7f6279dfd99b2f91dfa88fc42eb6f05f0a644ff0
Michel Martens <michel@soveran.com>
2010-04-28 14:49:07 -0300

    Fix settings in logger.

--- vendor/monk-glue/lib/monk/glue/logger.rb
+++ vendor/monk-glue/lib/monk/glue/logger.rb
@@ -4,7 +4,7 @@ require 'logger'
 def logger
   $logger ||= begin
     $logger = ::Logger.new(root_path("log", "#{RACK_ENV}.log"))
-    $logger.level = ::Logger.const_get((settings(:log_level) || :warn).to_s.upcase)
+    $logger.level = ::Logger.const_get((monk_settings(:log_level) || :warn).to_s.upcase)
     $logger.datetime_format = "%Y-%m-%d %H:%M:%S"
     $logger
   end
