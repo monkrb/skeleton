9fdbd98a09906cdba91e659ed431d2ec00638718
Cyril David <cyx.ucron@gmail.com>
2010-11-05 00:09:31 +0800

    Vendor nest 1.0.0, syntax 1.0.0 and maruku 0.6.0.

---
+++ vendor/nest-1.0.0/lib/nest.rb
@@ -0,0 +1,36 @@
+require "redis"
+
+class Nest < String
+  VERSION = "1.0.0"
+
+  METHODS = [:append, :blpop, :brpop, :decr, :decrby, :del, :exists,
+  :expire, :expireat, :get, :getset, :hdel, :hexists, :hget, :hgetall,
+  :hincrby, :hkeys, :hlen, :hmget, :hmset, :hset, :hsetnx, :hvals,
+  :incr, :incrby, :lindex, :linsert, :llen, :lpop, :lpush, :lpushx,
+  :lrange, :lrem, :lset, :ltrim, :move, :psubscribe, :publish,
+  :punsubscribe, :rename, :renamenx, :rpop, :rpoplpush, :rpush,
+  :rpushx, :sadd, :scard, :sdiff, :sdiffstore, :set, :setex, :setnx,
+  :sinter, :sinterstore, :sismember, :smembers, :smove, :sort,
+  :spop, :srandmember, :srem, :strlen, :subscribe, :substr, :sunion,
+  :sunionstore, :ttl, :type, :unsubscribe, :watch, :zadd, :zcard,
+  :zcount, :zincrby, :zinterstore, :zrange, :zrangebyscore, :zrank,
+  :zrem, :zremrangebyrank, :zremrangebyscore, :zrevrange, :zrevrank,
+  :zscore, :zunionstore]
+
+  attr :redis
+
+  def initialize(key, redis = Redis.connect)
+    super(key.to_s)
+    @redis = redis
+  end
+
+  def [](key)
+    self.class.new("#{self}:#{key}", redis)
+  end
+
+  METHODS.each do |meth|
+    define_method(meth) do |*args, &block|
+      redis.send(meth, self, *args, &block)
+    end
+  end
+end
